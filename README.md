70bcaecf57f8d9ce87c1a7852c02282479a1199a
DostonSuhrobov <doston.sanatovich@gmail.com>
2021-08-27 19:36:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qnlzg9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qnlzg9)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Heroku</h1>
+      <p>It is me Doston !</p>
+    </div>
+  );
+}
